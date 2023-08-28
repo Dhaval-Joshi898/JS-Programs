d69380c5ff7ef82672befc8530efc5f5d32d0539
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 00:27:40 +0530

    Do whike another example

--- Loops IN JS/doWhile.js
+++ Loops IN JS/doWhile.js
@@ -1,6 +1,11 @@
-do {
-     num = prompt("Enter the number:");
-     intnum=parseInt(num) 
+// do {
+//      num = prompt("Enter the number:");
+//      intnum=parseInt(num) 
 
-}
-while (intnum<= 50); 
+// }
+// while (intnum<= 50); 
+let num=0;
+do{
+    num+=1;
+    console.log(num)
+}while(num<=5)
