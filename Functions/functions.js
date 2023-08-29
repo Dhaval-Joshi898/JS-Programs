2d8d899c946831c9ce8eab5f5636e0297e6209a4
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 12:19:11 +0530

    Function with parameter and argument example

--- Functions/functions.js
+++ Functions/functions.js
@@ -7,3 +7,15 @@ function greetUser(){
 }
 
 greetUser()   //calling the function then only it will print .
+
+// Another function example
+
+//Function with PARAMETER
+function askUser(name){             //function askUser(parameter)
+    console.log("HEllo " +name + ",How are you?")
+}
+
+askUser("Dhaval")   //Calling function with argument
+
+
+// askUser(argument)   1parameter is given therefore it is required to provide one argument also.
