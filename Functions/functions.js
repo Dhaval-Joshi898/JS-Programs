bb805b61ab030ef13016944f09cf26d3ffce475b
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 12:14:50 +0530

    Function example

---
+++ Functions/functions.js
@@ -0,0 +1,9 @@
+/*  FUNCTIONS in JS*/
+
+// Creating function with "function" keyword
+
+function greetUser(){
+    console.log("Welcome to the page")
+}
+
+greetUser()   //calling the function then only it will print .
