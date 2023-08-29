a986f0866c58864a3aeb9389c9078d04cbc2e339
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 14:36:07 +0530

    default parameter

---
+++ Functions/defaultParam.js
@@ -0,0 +1,10 @@
+//Deafult parameter in function
+
+function  sum(num1,num2){
+    
+    // a="Addition of two number is:"+(num1+num2);
+    a=`Addition of two number is: ${num1+num2}`;   //using backtick you can also concatenate string and insert variable
+   
+
+}
+sum(2,3);
