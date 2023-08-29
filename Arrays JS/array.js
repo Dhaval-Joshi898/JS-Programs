f9ef31ad47c26a3672fc7f9e335bdf921721b44d
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 23:19:38 +0530

    Arrays declared,push and unshift methods

---
+++ Arrays JS/array.js
@@ -0,0 +1,19 @@
+let fruits=["Apples","Mango","Banana"]
+
+console.log(fruits)
+console.log(fruits[2])
+console.log(fruits[1])
+
+/*
+a=fruits.push("chikoo")          
+console.log(a)  //if print this it will give length of list after addingor pushing*/
+fruits.push("chikoo")   //if not want the return value o push
+console.log(fruits)   //chikoo is added 
+
+//Push will add items at the last of the list 
+
+
+/* To add items to the start of the list  we used unshift */
+fruits.unshift("Guava")
+console.log(fruits)
+
