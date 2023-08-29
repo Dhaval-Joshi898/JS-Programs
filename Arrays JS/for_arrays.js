1a0b03481de093e3877b539b3b4512f433773d3f
Dhaval <joshidhaval2002@gmail.com>
2023-08-30 02:05:54 +0530

    using for loop, iteraing array items

---
+++ Arrays JS/for_arrays.js
@@ -0,0 +1,9 @@
+let students = ['Raj', 'Rahul', "Roy", "Riya", "Alex"]
+
+for (let i=0;i<5;i++){
+    console.log(`Roll No ${i+1} is: ${students[i]}`)
+}
+
+// for (let i=0;i<students.length;i++){  //if arrays length not known use .length
+//     console.log(`Roll No ${i} is: ${students[i]}`)
+// }
