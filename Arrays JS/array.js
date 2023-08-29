6fd26463877f9b5ee2c0141fbb3f41bf7b9fd078
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 23:27:53 +0530

    pop and shift method in arrays

--- Arrays JS/array.js
+++ Arrays JS/array.js
@@ -1,19 +1,32 @@
 let fruits=["Apples","Mango","Banana"]
 
 console.log(fruits)
-console.log(fruits[2])
-console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[1])
 
 /*
 a=fruits.push("chikoo")          
 console.log(a)  //if print this it will give length of list after addingor pushing*/
-fruits.push("chikoo")   //if not want the return value o push
-console.log(fruits)   //chikoo is added 
+// fruits.push("chikoo")   //if not want the return value o push
+// console.log(fruits)   //chikoo is added 
 
 //Push will add items at the last of the list 
 
 
 /* To add items to the start of the list  we used unshift */
-fruits.unshift("Guava")
-console.log(fruits)
 
+
+// fruits.unshift("Guava")
+// console.log(fruits)
+                                            
+
+//Now to remove items form arrays
+
+vegetables=["onion","tomato","cabbage"]
+
+a=vegetables.pop()  //to remove items from the last 
+console.log(a)  // it will return the item it has removed from list
+console.log(vegetables)
+
+vegetables.shift()
+console.log(vegetables)  //to remove items form the first
