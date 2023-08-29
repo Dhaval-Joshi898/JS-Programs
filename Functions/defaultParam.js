0016b91d73a402d2b100d1b09f3bd3dba4ed9a12
Dhaval <joshidhaval2002@gmail.com>
2023-08-29 23:18:44 +0530

    function revoking without parameter

--- Functions/defaultParam.js
+++ Functions/defaultParam.js
@@ -1,10 +1,11 @@
 //Deafult parameter in function
 
-function  sum(num1,num2){
+function  sum(num1=0,num2=0){
     
     // a="Addition of two number is:"+(num1+num2);
-    a=`Addition of two number is: ${num1+num2}`;   //using backtick you can also concatenate string and insert variable
+    console.log(`Addition of two number is: ${num1+num2}`);   //using backtick you can also concatenate string and insert variable
    
 
 }
 sum(2,3);
+sum(); //here i have not given any argument to the function then it will consider the default argument
