27e958cb795263313bfcb5e88521c8b736dc0a46
Dhaval <joshidhaval2002@gmail.com>
2023-09-01 00:19:32 +0530

    Variable Hoisting

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,10 @@
+console.log(i)
+var i=5;
+//let i=5;   ReferenceError: Cannot access 'i' before initialization
+
+// greet()  
+
+// function greet(name){
+//     console.log(`Hello ${name},How are you?`)
+// }
+
