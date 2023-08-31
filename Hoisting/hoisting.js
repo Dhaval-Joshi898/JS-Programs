1324ed32e6c6d651c2cc5d8bd50523cd811d7ca3
Dhaval <joshidhaval2002@gmail.com>
2023-09-01 00:21:09 +0530

    function hoisting

--- Hoisting/hoisting.js
+++ Hoisting/hoisting.js
@@ -2,9 +2,9 @@ console.log(i)
 var i=5;
 //let i=5;   ReferenceError: Cannot access 'i' before initialization
 
-// greet()  
+greet()   //function hoisting invoking function before declaring
 
-// function greet(name){
-//     console.log(`Hello ${name},How are you?`)
-// }
+function greet(name){
+    console.log(`Hello ${name},How are you?`)
+}
 
