c0cdd7fb5b453e5a19dee232f27f0e83708ca88a
Dhaval <joshidhaval2002@gmail.com>
2023-09-01 00:23:08 +0530

    function hoisting op with and without parameters

--- Hoisting/hoisting.js
+++ Hoisting/hoisting.js
@@ -3,7 +3,11 @@ var i=5;
 //let i=5;   ReferenceError: Cannot access 'i' before initialization
 
 greet()   //function hoisting invoking function before declaring
+//O/P: Hello undefined,How are you?
 
+
+greet('Tom')
+// O/P : Hello Tom,How are you?
 function greet(name){
     console.log(`Hello ${name},How are you?`)
 }
